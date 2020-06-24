0b5502657b78d551f732c16cd2e40ae6e4fe0246
hero <hpky123@naver.com>
2020-06-24 23:08:48 +0900

    일단대충고침프로필

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
+
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
 };
@@ -41,6 +42,6 @@ export const logout = (req, res) => {
 
 
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
-export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
